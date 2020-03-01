744549075dff44d5f8e165bc005ab32801723c29
Jan Bajana <jan.bajana@gmail.com>
2020-03-01 16:08:48 +0100

    rename setup magnum extras build.

--- cmake-scripts/setup-magnum-extras-build.sh
+++ cmake-scripts/setup-magnum-extras-build.sh
@@ -2,10 +2,12 @@
 
 set -x
 
-SOURCES_FOLDER="$1" # input directory (example) /e/Git/build/
-BUILD_FOLDER="$2" # output directory (example) /e/Git/
-BUILD_TYPE="$3" # Release/Debug
-BUILD_ARCH="$4" # Android/Win64/linux
+SOURCES_FOLDER="$1" # root input directory (example) /e/Git
+BUILD_FOLDER="$2"   # build output directory (example) /e/Git/build/
+BUILD_TYPE="$3"     # Release/Debug
+BUILD_ARCH="$4"     # Android/Win64/linux
+PROCESS_BUILD="$5"  # ON/OFF
+
 SOURCE_PROJECT="magnum-extras"
 SOURCE_LOCATION="${SOURCES_FOLDER}/${SOURCE_PROJECT}"
 GENERATOR="Ninja"
@@ -13,52 +15,79 @@ GENERATOR="Ninja"
 BUILD_LOCATION=${BUILD_FOLDER}/${SOURCE_PROJECT}-build-${BUILD_ARCH}-${BUILD_TYPE}
 INSTALL_LOCATION=${BUILD_FOLDER}/install-${BUILD_ARCH}-${BUILD_TYPE}
 
+if [ "$BUILD_ARCH" = "Android" ]; then
+    BUILD_ABI="arm64-v8a" # arm64-v8a/armeabi-v7a (Android only)
+    BUILD_LOCATION=${BUILD_LOCATION}-${BUILD_ABI}
+fi
+
 mkdir -p "${BUILD_LOCATION}"
 rm -rf "${BUILD_LOCATION}"/*
 
-if [ "$BUILD_ARCH" = "Android" ] ; then
+if [ "$BUILD_ARCH" = "Android" ]; then
 
-ANDROID_NDK=${HOME}/AppData/Local/Android/Sdk/ndk/20.0.5594570
-# INSTALL_LOCATION=${ANDROID_NDK}/platforms/android-22/arch-arm64/usr
+if [[ -z "${ANDROID_NDK_HOME}" ]]; then
+    echo "ANDROID_NDK_HOME environment variable not set!"
+    exit 1
+fi
+    
+    ADDITIONAL_CMAKE_PARAMS=" \
+    "
 
-cmake -B"${BUILD_LOCATION}" -H"${SOURCE_LOCATION}" -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
-  -G"${GENERATOR}" \
-  -DCMAKE_INSTALL_PREFIX="${INSTALL_LOCATION}" \
-  -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK}/build/cmake/android.toolchain.cmake \
-  -DCMAKE_ANDROID_NDK=${ANDROID_NDK} \
-  -DCMAKE_SYSTEM_NAME=Android \
-  -DCMAKE_SYSTEM_VERSION=26 \
-  -DCMAKE_ANDROID_ARCH_ABI=arm64-v8a \
-  -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang \
-  -DCMAKE_ANDROID_STL_TYPE=c++_static \
-  -DCORRADE_RC_EXECUTABLE="/c/Corrade/bin/corrade-rc.exe" \
-  -DCMAKE_FIND_ROOT_PATH="${BUILD_FOLDER}/install-${BUILD_ARCH}-${BUILD_TYPE}" \
-  -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+    cmake -B"${BUILD_LOCATION}" -H"${SOURCE_LOCATION}" -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
+        -G"${GENERATOR}" \
+        -DCMAKE_INSTALL_PREFIX="${INSTALL_LOCATION}" \
+        -DCMAKE_TOOLCHAIN_FILE="${ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake" \
+        -DCMAKE_ANDROID_NDK="${ANDROID_NDK_HOME}" \
+        -DCMAKE_SYSTEM_NAME=Android \
+        -DCMAKE_SYSTEM_VERSION=26 \
+        -DCMAKE_ANDROID_ARCH_ABI=${BUILD_ABI} \
+        -DANDROID_NATIVE_API_LEVEL=26 \
+        -DANDROID_ABI=${BUILD_ABI} \
+        -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang \
+        -DCMAKE_ANDROID_STL_TYPE=c++_static \
+        -DCMAKE_FIND_ROOT_PATH="${BUILD_FOLDER}/install-${BUILD_ARCH}-${BUILD_TYPE}" \
+        -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+        ${ADDITIONAL_CMAKE_PARAMS}
 
-#  -D_CORRADE_MODULE_DIR="${ANDROID_NDK}/sysroot/usr/share/cmake/Corrade" \
-#  -DMAGNUM_INCLUDE_INSTALL_PREFIX="${ANDROID_NDK}/sysroot/usr" \
+elif [ "$BUILD_ARCH" = "Win64" ]; then
 
-elif [ "$BUILD_ARCH" = "Win64" ] ; then
+    GENERATOR="Visual Studio 16 2019"
 
-GENERATOR="Visual Studio 15 2017 Win64"
+    ADDITIONAL_CMAKE_PARAMS=" \
+        -DBUILD_PLUGINS_STATIC=OFF
+    "
 
-cmake -B"${BUILD_LOCATION}" -H"${SOURCE_LOCATION}" -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
-  -G"${GENERATOR}" \
-  -DCMAKE_INSTALL_PREFIX="${INSTALL_LOCATION}" \
-  -DCMAKE_FIND_ROOT_PATH="${BUILD_FOLDER}/install-${BUILD_ARCH}-${BUILD_TYPE}" \
-  -DIMGUI_DIR="${SOURCES_FOLDER}/imgui" \
-  -DCMAKE_RUNTIME_OUTPUT_DIRECTORY="bin" \
-  -DWITH_IMGUI=ON
+elif [ "$BUILD_ARCH" = "macOS" ]; then
 
-else
+# GENERATOR=Xcode
 
-ADDITIONAL_CMAKE_PARAMS=""
-BUILD_ARCH="x86"
-#export CC=/usr/bin/clang
-#export CXX=/usr/bin/clang++
+    ADDITIONAL_CMAKE_PARAMS=" \
+        -DBUILD_PLUGINS_STATIC=OFF
+    "
+else #expected Linux x86
 
+    export CC=/usr/bin/clang
+    export CXX=/usr/bin/clang++
+
+    ADDITIONAL_CMAKE_PARAMS=" \
+        -DBUILD_PLUGINS_STATIC=OFF
+    "
 fi
 
-#cd ${BUILD_LOCATION}
-#cmake --build . --config ${BUILD_TYPE}
-#cmake --build . --config ${BUILD_TYPE} --target install
+cmake -B"${BUILD_LOCATION}" -H"${SOURCE_LOCATION}" -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
+    -G"${GENERATOR}" \
+    -DCMAKE_INSTALL_PREFIX="${INSTALL_LOCATION}" \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+    -DCMAKE_RUNTIME_OUTPUT_DIRECTORY="bin" \
+    -DWITH_PLAYER=ON \
+    -DWITH_UI=ON \
+    -DWITH_UI_GALLERY=ON \
+    -DBUILD_TESTS=OFF \
+    -DBUILD_GL_TESTS=OFF \
+    -DBUILD_STATIC=ON \
+    ${ADDITIONAL_CMAKE_PARAMS}
+
+if [ "$PROCESS_BUILD" = "ON" ]; then
+    cd ${BUILD_LOCATION}
+    cmake --build . --config ${BUILD_TYPE} --target install
+fi
