710f8e6b340c337aa0434c1b3bf6dabc695f3a14
Jan Bajana <jbajana@snapchat.com>
2020-03-02 08:11:55 +0100

    assimp build script changes.

--- cmake-scripts/setup-assimp-build.sh
+++ cmake-scripts/setup-assimp-build.sh
@@ -5,30 +5,48 @@ set -x
 SOURCES_FOLDER="$1" # input directory (example) /e/Git
 BUILD_FOLDER="$2"   # output directory (example) /e/Git/build
 BUILD_TYPE="$3"     # Release/Debug
-BUILD_ARCH="$4"     # Android/Win64/linux
+BUILD_SYSTEM="$4"     # Android/Win64/linux
 PROCESS_BUILD="$5"  # ON/OFF build or configurtion only
+BUILD_ARCH=arm64-v8a     # arm64-v8a/armeabi-v7a (Android only)
 
 SOURCE_PROJECT="assimp"
 SOURCE_LOCATION="${SOURCES_FOLDER}/${SOURCE_PROJECT}"
 
 GENERATOR="Ninja"
 
-BUILD_LOCATION=${BUILD_FOLDER}/${SOURCE_PROJECT}-build-${BUILD_ARCH}-${BUILD_TYPE}
-INSTALL_LOCATION=${BUILD_FOLDER}/install-${BUILD_ARCH}-${BUILD_TYPE}
+BUILD_LOCATION=${BUILD_FOLDER}/${SOURCE_PROJECT}-build-${BUILD_SYSTEM}-${BUILD_TYPE}
+INSTALL_LOCATION=${BUILD_FOLDER}/${SOURCE_PROJECT}-install-${BUILD_SYSTEM}-${BUILD_TYPE}
 
-if [ "$BUILD_ARCH" = "Android" ]; then
-    BUILD_ABI="arm64-v8a" # arm64-v8a/armeabi-v7a (Android only)
-    BUILD_LOCATION=${BUILD_LOCATION}-${BUILD_ABI}
+if [ "$BUILD_SYSTEM" = "Android" ]; then
+    BUILD_LOCATION=${BUILD_LOCATION}-${BUILD_ARCH}
 fi
 
 mkdir -p "${BUILD_LOCATION}"
 rm -rf "${BUILD_LOCATION}"/*
 
-if [ "$BUILD_ARCH" = "Android" ] ; then
+if [ "$BUILD_SYSTEM" = "Android" ] ; then
 
-  ADDITIONAL_CMAKE_PARAMS="-DCMAKE_TOOLCHAIN_FILE=/usr/local/vos-x86_64/DAQRI-VOS-Toolchain-GCC.cmake"
 
-elif [ "$BUILD_ARCH" = "Win64" ] ; then
+    if [[ -z "${ANDROID_NDK_HOME}" ]]; then
+        echo "ANDROID_NDK_HOME environment variable not set!"
+        exit 1
+    fi
+    
+  ADDITIONAL_CMAKE_PARAMS=" \
+        -DCMAKE_TOOLCHAIN_FILE="${ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake" \
+        -DCMAKE_ANDROID_NDK="${ANDROID_NDK_HOME}" \
+        -DCMAKE_SYSTEM_NAME=Android \
+        -DCMAKE_SYSTEM_VERSION=28 \
+        -DCMAKE_ANDROID_ARCH_ABI=${BUILD_ARCH} \
+        -DANDROID_ABI=${BUILD_ARCH} \
+        -DANDROID_NATIVE_API_LEVEL=28 \
+        -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang \
+        -DCMAKE_ANDROID_STL_TYPE=c++_static \
+        -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+        -DASSIMP_BUILD_TESTS=OFF \
+  "
+
+elif [ "$BUILD_SYSTEM" = "Win64" ] ; then
 
   ADDITIONAL_CMAKE_PARAMS=" \
     -A Win32 \
@@ -38,7 +56,7 @@ elif [ "$BUILD_ARCH" = "Win64" ] ; then
 else
 
   ADDITIONAL_CMAKE_PARAMS=""
-  BUILD_ARCH="x86"
+  BUILD_SYSTEM="x86"
 
   export CC=/usr/bin/clang
   export CXX=/usr/bin/clang++
